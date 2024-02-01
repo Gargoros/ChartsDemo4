60e8a0b2f6cd87be226dc4b2b6f0c735cfb92381
MIKHAIL ZHACHKO <mikhailzhachko@gmail.com>
2024-02-01 17:41:50 +0300

    Initial Commit

---
+++ ChartsDemo4/ChartsDemo4App.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsDemo4App.swift
+//  ChartsDemo4
+//
+//  Created by MIKHAIL ZHACHKO on 1.02.24.
+//
+
+import SwiftUI
+
+@main
+struct ChartsDemo4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
